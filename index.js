fad1dcf6d3055bd592faad70d21034edcd9cada1
Rebbecca Fox <foxy2530@gmail.com>
2021-10-22 04:14:05 +0100

    secruity app

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const yargs =require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const bcrypt = require('bcrypt');
+
+const main = async () => {
+    const argv = yargs(hideBin(process.argv)).argv;
+    const saltRounds = parseInt(process.env.SALT_ROUNDS);
+    const username = argv.username;
+    const password = argv.password;
+    const otherPassword = argv.otherPassword;
+
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(password, salt);
+    const matched = await bcrypt.compare(otherPassword, hash);
+    
+    if(matched) {
+        console.log("Hashed passwords match");
+    } else {
+        console.log("Hashed passwords do not match");
+    }
+}
+
+main();
